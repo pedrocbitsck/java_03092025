92acf04dc5efcc5fe69ef60949c4c3c4483ece88
Pedro Cícero Oliveira Bitsck <pedrocbitsck10@gmail.com>
2025-09-03 20:17:03 -0300

    Add files via upload

---
+++ ex2.java
@@ -0,0 +1,8 @@
+package aula_4;
+public class ex2 {
+        public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
